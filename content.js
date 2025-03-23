83289eccce291fadc3e772cf854b5104c13175b6
Kyle Fardy <fardy662@gmail.com>
2025-03-23 12:25:34 +0000

    fixes the patch by UM

--- content.js
+++ content.js
@@ -1,47 +1,47 @@
 window.onload = function () {
-    console.log("Union Mods Premium Extension Loaded");
-    console.log("Thank You For Using Our Extension!");
+  console.log('Union Mods Premium Extension Loaded');
+  console.log('Thank You For Using Our Extension!');
+  try {
+    remove_ads();
+  } catch (e) {
+    console.log('Error Removing Ads:', e);
+  }
+  const is_file_page = document.querySelector('#prime-link');
+  if (is_file_page) {
+    console.log('Files Found, Running URL Replacement');
     try {
-        remove_ads();
+      replace_download_url();
     } catch (e) {
-        console.error("Error Removing Ads:", e);
+      console.log('Error Replacing Download URL:', e);
     }
-    const is_file_page = document.querySelector("#download-btn");
-    if (is_file_page) {
-        console.log("Files Found, Running URL Replacement");
-        try {
-            replace_download_url();
-        } catch (e) {
-            console.error("Error Replacing Download URL:", e);
-        }
-    } else {
-        console.log("No Files Found On The Page");
-    }
-    add_image();
+  } else {
+    console.log('No Files Found On The Page');
+  }
 };
 function remove_ads() {
-    document.querySelectorAll('li.c-card').forEach(card => {
-        if (card.querySelector('video') || card.querySelector('.c-card__detail') && card.querySelector('.c-card__detail').textContent.includes('Promoted by')) {
-            console.log("Removing AD:", card);
-            card.remove();
-        }
-    });
+  document
+    .querySelector('body > main > div.row.u-padding-sides-40')
+    .classList.add('hidden');
+  document.querySelectorAll('li.c-card').forEach((card) => {
+    if (
+      card.querySelector('video') ||
+      (card.querySelector('.c-card__detail') &&
+        card
+          .querySelector('.c-card__detail')
+          .textContent.includes('Promoted by'))
+    ) {
+      console.log('Removing AD:', card);
+      card.remove();
+    }
+  });
 }
 function replace_download_url() {
-    const downloadBtn = document.querySelector("#download-btn");
-    if (downloadBtn && downloadBtn.href) {
-        if (downloadBtn.href.match(/file=(\d+)/)) {
-            downloadBtn.href = `https://unionmods.com/?API=FileDownload&fileID=${downloadBtn.href.match(/file=(\d+)/)[1]}`;
-        }
-    }
-}
-function add_image() {
-    const img = document.createElement('img');
-    img.src = chrome.runtime.getURL('images/logo.png'); // Path to your image file in the extension directory
-    img.alt = 'Description of Image';
-    img.style.width = '100px'; // Adjust size as needed
-    img.style.height = 'auto';
-
-    // Append the image to a specific element, e.g., body
-    document.body.appendChild(img);
+  document
+    .querySelector('body > main > section > div > div > div:nth-child(2)')
+    .classList.add('hidden');
+  document.querySelector('#prime-link').classList.add('hidden');
+  document.querySelector('#download-link').classList.remove('hidden');
+  document.querySelector(
+    'body > main > section > div > div > div:nth-child(3)'
+  ).style.marginTop = '20px';
 }
